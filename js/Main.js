2558506865dcc9d6d574688bcd00161ad4bc2cfd
James <james.hudgins.work@gmail.com>
2020-06-16 00:09:41 -0400

    simplified to loop for each item and added logic for textarea. Next will be the save buttons.

--- js/Main.js
+++ js/Main.js
@@ -2,96 +2,67 @@ $(document).ready(function () {
     console.log(moment())
     // moment object
     var m = moment()
-    var today = m.format("LL")
-    var workHours = [m.startOf('day').add(9, 'hours').format("LT"),m.startOf('day').add(10, 'hours').format("LT"),m.startOf('day').add(11, 'hours').format("LT"),m.startOf('day').add(12, 'hours').format("LT"),m.startOf('day').add(13, 'hours').format("LT"),m.startOf('day').add(14, 'hours').format("LT"),m.startOf('day').add(15, 'hours').format("LT"),m.startOf('day').add(16, 'hours').format("LT"),m.startOf('day').add(17, 'hours').format("LT")]
-    
+    var today = m.format("hh:mm")
+    var workHours = [m.startOf('day').add(9, 'hours').format("hh:mm"),m.startOf('day').add(10, 'hours').format("hh:mm"),m.startOf('day').add(11, 'hours').format("hh:mm"),m.startOf('day').add(12, 'hours').format("hh:mm"),m.startOf('day').add(13, 'hours').format("hh:mm"),m.startOf('day').add(14, 'hours').format("hh:mm"),m.startOf('day').add(15, 'hours').format("hh:mm"),m.startOf('day').add(16, 'hours').format("hh:mm"),m.startOf('day').add(17, 'hours').format("hh:mm")]
+     // var for today display
+     var todayDisplay = $("#today")
+    todayDisplay.text(today)
     // placeholder for task lists
-    var tasks = [["task1","task2","some other task "],["wow a task","omg another"],[],[],[],[],[]]
-    
-    console.log(tasks[0][0])
-    // var for today display
-    var todayDisplay = $("#today")
-    // ident task list (ul)
-    var taskList = $("#task-list")
+    console.log(today)
+    // ident task list 
+    var taskList = $("#taskList")
 
-    //initialize check for existing tasks
-    // init();
-
+    // initialize check for existing tasks
+    init();
     
     function renderWorkHours() {
+        // clear out the taskLisk
+        taskList.empty()
             // loop through for each hour of the work day
-    for (i = 0; i < workHours.length-1; i++) {
-        console.log(workHours[i])
-     //  create a new link 
-     var a = $("<a>")
-     // add classes to the anchor tag.
-     a.addClass("list-group-item list-group-item-action");
-     
-     // create a new div to go inside the new anchor tag
-     var newDiv = $("<div>")
-     // add classes to the div
-     newDiv.addClass("d-flex w-100 justify-content-between")
- 
-     // create List group item heading to append to the div
-     h5 = $("<h5>")
-     // add classes to the heading
-     h5.addClass("mb-1")
-     //add text to the list item
-     h5.text("")
-     
-     // create new small
-     small = $("<small>")
-     // add text to the list item
-     small.text(workHours[i] + " - " + workHours[i+1] )
- 
-     // append the header and small to the the div and then the div to the anchor
-     h5.appendTo(newDiv)
-     small.appendTo(newDiv);
-     newDiv.appendTo(a)
- 
-     // lastly append the div to the work hour container
-     a.appendTo("#workHourContainer")
-     
-    
-     // create a new ul as placeholder for tasks
-     var ul = $("<ul>")
-     // add class
-     ul.addClass("mb-1")
-     // add some text
-     ul.attr("time-block", + i);
-     ul.appendTo(a)
- 
-            // Clear task list
-            taskList.html("")
-            // Render a new li for each task in each time block
-            for (var j = 0; j < tasks[i].length; j++) {
-            var task = tasks[i][j];
-            // Create a new list item for each work hour
-            var li = $("<li>");
-            li.text(tasks[i][j]);
-            li.attr("data-index", j)
-            // create a delete button for the item
-            var button = $("<button>")
-            // add text to the button
-            button.text("Delete")
-            // append the button to the current task
-            button.appendTo(li)
-            // append the task li to the time-block
-            li.appendTo(ul);
-            }
-    }
-    }
-    renderWorkHours()
-    
-    todayDisplay.text(today)
-    
-    // check for existing tasks
-        // if they exist load them
-        // else if they don't - do nothing
-    
-        
-    // populate each task (if saved)
+            for (i = 0; i < workHours.length-1; i++) {
+            // console.log(workHours[i])
+            //  create new div 
+            var newDiv = $("<div>")
+            // add class to the new div
+            newDiv.addClass("row time-block")
+            // next add a label for the time block
+            var label = $("<label>")
+            // add class to the label
+            // label.addClass("description")
+            // now add text to the label
+            label.text(workHours[i] + " - " + workHours[i+1])
+            // now we add a textarea
+            var textArea = $("<textarea>")
+            // add attr to the textarea
 
+            // if text-block date value you is less than current time add the past class to the textarea
+            if (workHours[i+1] > today == true) {
+                textArea.addClass("past")
+            }
+            // if current text-block value is greater than today add future class to the textarea
+            else if (today > workHours[i] == true) {
+
+                textArea.addClass("future")
+            }
+            // if the the current text-block value is between the current time add the present class to the textarea
+            else if (workHours[i+1] > today && today > workHours[i] == true) {
+                textArea.addClass("present")
+            } 
+            textArea.attr("cols", 50)
+            // now we add the button
+            var button = $("<button>")
+            button.addClass("saveBtn")
+            button.text("Save")
+            button.attr("type", "submit")
+            // append the created elements to the new div
+            label.appendTo(newDiv);
+            textArea.appendTo(newDiv);
+            button.appendTo(newDiv);
+            // append the div to the taskList
+            newDiv.appendTo(taskList)
+            }
+
+    }
 
     // Check for existing entires
     function init() {
@@ -100,23 +71,44 @@ $(document).ready(function () {
         var storedTasks = JSON.parse(localStorage.getItem("tasks"));
       
         // If tasks are retrieved from localStorage, update the task array
-        if (storedTodos !== null) {
-          tasks = storedtasks;
+        if (storedTasks !== "") {
+          tasks = storedTasks;
         }
       
         // Render tasks to the DOM
         renderWorkHours()
+        storeTask()
     }
-
+    function storeTask() {
+            // Stringify and set "todos" key in localStorage to todos array
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
     // when a timeblock is selected
-    $("a").on("click", function () {
-        console.log(this)
+    $("textarea").on("click", function () {
+        // console.log(this.value)
     });
 
-    function storeTask() {
-        // Stringify and set "todos" key in localStorage to todos array
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-      }
+    // // when the save button is clicked
+    // $(selector).submit(function (e) { 
+    //     e.preventDefault();
+
+    //     var TaskText = TaskInput.value.trim();
+      
+    //     // Return from function early if submitted task is blank
+    //     if (TaskText === "") {
+    //       return;
+    //     }
+      
+    //     // Add new TaskText to Tasks array, clear the input
+    //     Tasks.push(TaskText);
+    //     TaskInput.value = "";
+      
+    //     // Store updated Tasks in localStorage, re-render the list
+    //     storeTasks();
+    //     renderTasks();
+        
+    // });
+  
 
 
 });
